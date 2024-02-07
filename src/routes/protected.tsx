21874d788b33a76ac42b69b4fd9c0e5a70dc4e1b
Artifex <mac@mkra.dev>
2024-02-08 03:27:11 +0800

    fix: redirect not working, using back Navigate with replace = true props

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,12 +1,12 @@
 import { PageLoader } from "@/components/page-loader";
 import { useAuth0 } from "@auth0/auth0-react";
-import { redirect, Outlet } from "react-router-dom";
+import { redirect, Outlet, Navigate } from "react-router-dom";
 
 export const Protected = () => {
   const { isAuthenticated, isLoading } = useAuth0();
 
   if (!isAuthenticated) {
-    redirect("/");
+    return <Navigate to="/" replace={true} />;
   }
 
   if (isLoading) {
