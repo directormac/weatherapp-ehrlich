0292e26684a6d1ffd9e30abd8edd87d5322b41f5
Artifex <mac@mkra.dev>
2024-02-08 01:26:40 +0800

    refactor: remove Duplicate ThemeProvider wrapping twice

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "@/components/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 import { useAuth0 } from "@auth0/auth0-react";
 import { PageLoader } from "@/components/page-loader";
 
@@ -8,9 +7,9 @@ export const Layout = () => {
   const { isLoading } = useAuth0();
 
   return (
-    <ThemeProvider>
+    <>
       <Navbar />
       {isLoading ? <PageLoader /> : <Outlet />}
-    </ThemeProvider>
+    </>
   );
 };
