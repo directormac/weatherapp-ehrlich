54aeab14bd90f357de5c77008fcc98faf20a09e0
Artifex <mac@mkra.dev>
2024-02-08 03:34:26 +0800

    chore: remove unused import

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,6 +1,6 @@
 import { PageLoader } from "@/components/page-loader";
 import { useAuth0 } from "@auth0/auth0-react";
-import { redirect, Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 export const Protected = () => {
   const { isAuthenticated, isLoading } = useAuth0();
