39e3a9b7c97e759e598d0ac12b265c8df234278f
Artifex <mac@mkra.dev>
2024-02-08 04:15:35 +0800

    feat: load data from useLoaderData

--- src/components/search-weather.tsx
+++ src/components/search-weather.tsx
@@ -1,14 +1,27 @@
 import { Button } from "./ui/button";
 import { Form } from "react-router-dom";
 import { Input } from "./ui/input";
+import { SearchIcon } from "lucide-react";
 
 export const SearchWeather = () => {
   return (
-    <>
-      <Form method="post" action="/home">
-        <Input name="city" placeholder="City" />
+    <div className="flex flex-col justify-center items-center">
+      <Form
+        method="post"
+        action="/home"
+        className="flex flex-col w-3/6 justify-center items-center  space-y-4"
+      >
+        <div className="relative flex items-center w-full">
+          <SearchIcon className="absolute ml-2" />
+          <Input
+            name="city"
+            type="search"
+            placeholder="City"
+            className="pl-8"
+          />
+        </div>
         <Button type="submit">Display Weather</Button>
       </Form>
-    </>
+    </div>
   );
 };
--- src/routes/weather.tsx
+++ src/routes/weather.tsx
@@ -1,15 +1,47 @@
-import { loader } from "@/data/weather";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useLoaderData } from "react-router-dom";
+import React from "react";
 
 export const Weather = () => {
-  const city = useLoaderData() as Awaited<ReturnType<typeof loader>>;
-
-  console.log(city);
+  const city = useLoaderData();
 
   return (
-    <div>
-      {JSON.stringify(city)}
-      <h1>Weather Page</h1>
+    <div className="container">
+      <div className="flex flex-col space-y-8 py-20">
+        <h1>Weather Page</h1>
+        <Table>
+          <TableCaption>Weather for the City of {city?.name}</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Date(mm/dd/yyyy)</TableHead>
+              <TableHead>Temp(F)</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Main</TableHead>
+              <TableHead>Pressure</TableHead>
+              <TableHead>Humidity</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>{city?.dt}</TableCell>
+              <TableCell>{city?.main?.temp}</TableCell>
+              <TableCell>{city?.weather[0]?.description}</TableCell>
+              <TableCell>{city?.weather[0]?.main}</TableCell>
+              <TableCell>{city?.main?.pressure}</TableCell>
+              <TableCell>{city?.main?.humidity}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        {/* <p>{JSON.stringify(city)}</p> */}
+      </div>
     </div>
   );
 };
