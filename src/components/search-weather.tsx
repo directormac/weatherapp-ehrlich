96da66a96b2f8956bcefae2c30d7af2c706741f1
Artifex <mac@mkra.dev>
2024-02-08 14:15:23 +0800

    feat: form displaying errors from action

--- src/components/search-weather.tsx
+++ src/components/search-weather.tsx
@@ -1,14 +1,16 @@
 import { Button } from "./ui/button";
-import { Form } from "react-router-dom";
+import { Form, useActionData } from "react-router-dom";
 import { Input } from "./ui/input";
 import { SearchIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 export const SearchWeather = () => {
+  const errors = useActionData() as { city: string } | undefined;
+
   return (
     <div className="flex flex-col justify-center items-center">
       <Form
         method="post"
-        action="/home"
         className="flex flex-col w-3/6 justify-center items-center  space-y-4"
       >
         <div className="relative flex items-center w-full">
@@ -17,9 +19,10 @@ export const SearchWeather = () => {
             name="city"
             type="search"
             placeholder="City"
-            className="pl-8"
+            className={cn("pl-8", errors?.city ? "border-red-500" : "")}
           />
         </div>
+        {errors?.city && <p className="text-red-500 text-sm">{errors.city}</p>}
         <Button type="submit">Display Weather</Button>
       </Form>
     </div>
