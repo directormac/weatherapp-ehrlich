a9c670db691689bed6fd2799547bc8bcea6e8d2d
Artifex <mac@mkra.dev>
2024-02-08 14:18:32 +0800

    refactor: landing to not redirect authenticated user

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -1,13 +1,9 @@
 import { Button } from "@/components/ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
-
-  if (isAuthenticated) {
-    return <Navigate to="/home" replace />;
-  }
 
   const handleLogin = async () => {
     await loginWithRedirect({
@@ -18,14 +14,32 @@ export const Landing = () => {
   };
 
   return (
-    <div className="flex w-full mx-auto flex-col space-y-12 py-20 justify-center">
-      <p>
-        Welcome to the weather forecast web application. Please login with your
-        Github user to use the application and view the weather in your city.
-      </p>
-      <Button onClick={handleLogin} className="w-24">
-        Login
-      </Button>
+    <div className="container">
+      <div className="flex flex-col space-y-12 py-20 ">
+        {isAuthenticated ? (
+          <>
+            <p className="text-center">
+              {" "}
+              You are already logged in please navigate to the{" "}
+              <Link className="hover:underline" to="/home">
+                home
+              </Link>{" "}
+              page and search for a city, to find its weather forecasts!
+            </p>
+          </>
+        ) : (
+          <>
+            <p>
+              Welcome to the weather forecast web application. Please login with
+              your Github user to use the application and view the weather in
+              your city.
+            </p>
+            <Button onClick={handleLogin} className="w-24">
+              Login
+            </Button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
